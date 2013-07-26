6f14c973d58bcacda81803fe526fbeab8100a1be
Brett Linn <br3ttlinn@gmail.com>
2013-07-26 10:10:48 -0400

    Adding Impersonation

---
+++ SecurityProj2/SecurityProj2/Models/ProjectItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecurityProj2.Models
+{
+    public class ProjectItems
+    {
+        public string Title { get; set; }
+    }
+}
