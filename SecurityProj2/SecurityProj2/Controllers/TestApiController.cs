d681c2b346373fedbcf38bb8954d281b96239f7c
Brett Linn <br3ttlinn@gmail.com>
2013-07-26 09:57:51 -0400

    test webApi

---
+++ SecurityProj2/SecurityProj2/Controllers/TestApiController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SecurityProj2.Controllers
+{
+    public class TestApiController : ApiController
+    {
+        private string tfsTeamProjectCollectionUrl = "https://code.allegient.com/tfs/crm";
+        // GET api/testapi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/testapi
+        public IEnumerable<string> GetTest()
+        {
+            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri(tfsTeamProjectCollectionUrl));
+            foreach (var p in tfs.GetService<VersionControlServer>().GetAllTeamProjects(false))
+                yield return p.Name;
+        }
+
+        // GET api/testapi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/testapi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/testapi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/testapi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
